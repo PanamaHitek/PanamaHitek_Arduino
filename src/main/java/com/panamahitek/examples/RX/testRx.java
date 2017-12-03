86804df9a037b01c7099c55d04c1a16c37e7ab6f
Antony Garcia <Antony Garcia@SonyVaio>
2017-12-02 22:01:38 -0500

    Agregando ejemplos de uso a la libreria

---
+++ src/main/java/com/panamahitek/examples/RX/testRx.java
@@ -0,0 +1,56 @@
+package com.panamahitek.examples.RX;
+
+import com.panamahitek.ArduinoException;
+import com.panamahitek.PanamaHitek_Arduino;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
+/**
+ * @author Antony Garcia Gonzalez Un codigo que permite recibir datos desde el
+ * Arduino
+ */
+public class testRx {
+//Se crea una variable tipo PanamaHitek_Arduino
+
+    static PanamaHitek_Arduino ino = new PanamaHitek_Arduino();
+    //Se crea un eventListener para el puerto serie
+    static SerialPortEventListener listener = new SerialPortEventListener() {
+        @Override
+        //Si se recibe algun dato en el puerto serie, se ejecuta el siguiente metodo
+        public void serialEvent(SerialPortEvent serialPortEvent) {
+            try {
+                /*
+                Los datos en el puerto serie se envian caracter por caracter. Si se
+                desea esperar a terminar de recibir el mensaje antes de imprimirlo, 
+                el metodo isMessageAvailable() devolvera TRUE cuando se haya terminado
+                de recibir el mensaje, el cual podra ser impreso a traves del metodo
+                printMessage()
+                 */
+                if (ino.isMessageAvailable()) {
+                    //Se le asigna el mensaje recibido a la variable msg
+                    String msg = ino.printMessage();
+                    //Se imprime la variable msg
+                    System.out.println("Mensaje recibido --> " + msg);
+                }
+            } catch (SerialPortException ex) {
+                Logger.getLogger(testRx.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ArduinoException ex) {
+                Logger.getLogger(testRx.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    };
+
+    public static void main(String[] args) {
+        try {
+            //Se inicializa la conexion con el Arduino en el puerto COM5
+            ino.arduinoRX("COM5", 9600, listener);
+        } catch (ArduinoException ex) {
+            Logger.getLogger(testRx.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SerialPortException ex) {
+            Logger.getLogger(testRx.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
